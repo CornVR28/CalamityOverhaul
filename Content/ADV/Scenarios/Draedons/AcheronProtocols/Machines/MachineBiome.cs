26221a17c8c3ffbb606f54d34307fddcaf50d3d9
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-02-20 22:48:48 +0800

    update

--- Content/ADV/Scenarios/Draedons/AcheronProtocols/Machines/MachineBiome.cs
+++ Content/ADV/Scenarios/Draedons/AcheronProtocols/Machines/MachineBiome.cs
@@ -10,9 +10,6 @@ namespace CalamityOverhaul.Content.ADV.Scenarios.Draedons.AcheronProtocols.Machi
         public override ModWaterStyle WaterStyle => ModContent.GetInstance<MachineWater>();
         public override int BiomeTorchItemType => TorchID.Green;
         public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
-        public override string BestiaryIcon => "DimensionalRelease/Content/Distortions/DistortionBiome_Icon";
-        public override string BackgroundPath => "DimensionalRelease/Content/Machines/MachineBiome_Background";
-        public override string MapBackground => "DimensionalRelease/Content/Machines/MachineBiome_Background";
         public override bool IsBiomeActive(Player player) => MachineWorld.Active;
     }
 }
---
+++ Content/ADV/Scenarios/Draedons/AcheronProtocols/Machines/MachineSky.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.ADV.Scenarios.Draedons.AcheronProtocols.Machines
+{
+    internal class MachineSkyData : ModSceneEffect
+    {
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+        public override bool IsSceneEffectActive(Player player) => MachineWorld.Active;
+    }
+
+    internal class MachineSky
+    {
+    }
+}
