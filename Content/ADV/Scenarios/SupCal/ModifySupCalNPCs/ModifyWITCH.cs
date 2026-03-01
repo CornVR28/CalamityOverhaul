8da5c81e71e23a0a88a62672ddfbc9e60dd4fdf3
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-03-02 02:26:08 +0800

    Update ModifyWITCH.cs

--- Content/ADV/Scenarios/SupCal/ModifySupCalNPCs/ModifyWITCH.cs
+++ Content/ADV/Scenarios/SupCal/ModifySupCalNPCs/ModifyWITCH.cs
@@ -47,6 +47,7 @@ namespace CalamityOverhaul.Content.ADV.Scenarios.SupCal.ModifySupCalNPCs
         }
 
         public override void SetStaticDefaults() {
+            Homeless.Clear();
             Homeless.Add(this.GetLocalization("Homeless0"));
             Homeless.Add(this.GetLocalization("Homeless1"));
             Homeless.Add(this.GetLocalization("Homeless2"));
@@ -101,8 +102,10 @@ namespace CalamityOverhaul.Content.ADV.Scenarios.SupCal.ModifySupCalNPCs
         }
 
         public override bool AI() {
-            //考虑到总是有人遇到无法正确设置击败状态的问题，这里直接让女巫的AI里设置击败状态，确保只要女巫存在了，就一定会设置击败状态
-            CWRRef.SetDownedCalamitas(true);
+            if (!CWRRef.GetDownedCalamitas()) {
+                //考虑到总是有人遇到无法正确设置击败状态的问题，这里直接让女巫的AI里设置击败状态，确保只要女巫存在了，就一定会设置击败状态
+                CWRRef.SetDownedCalamitas(true);
+            }
             return true;
         }
     }
