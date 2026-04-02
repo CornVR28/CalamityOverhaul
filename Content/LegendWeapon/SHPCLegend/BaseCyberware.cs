aef66b75c80828562bae2509e4d16e1e14b86292
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-04-03 00:42:55 +0800

    update

---
+++ Content/LegendWeapon/SHPCLegend/BaseCyberware.cs
@@ -0,0 +1,8 @@
+﻿using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.LegendWeapon.SHPCLegend.Cyberwares.CyberItems
+{
+    internal abstract class BaseCyberware : ModItem
+    {
+    }
+}
---
+++ Content/LegendWeapon/SHPCLegend/Skills/Sandevistans/SandevistansItem.cs
@@ -0,0 +1,9 @@
+﻿using CalamityOverhaul.Content.LegendWeapon.SHPCLegend.Cyberwares.CyberItems;
+
+namespace CalamityOverhaul.Content.LegendWeapon.SHPCLegend.Skills.Sandevistans
+{
+    internal class SandevistansItem : BaseCyberware
+    {
+        public override string Texture => CWRConstant.Other + "SandevistansItem";
+    }
+}
