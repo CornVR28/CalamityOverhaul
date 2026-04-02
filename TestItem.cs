19732d7cc01b6ae18a32c1a09dfd5305c25338cc
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-04-02 16:26:41 +0800

    Update TestItem.cs

--- TestItem.cs
+++ TestItem.cs
@@ -86,13 +86,13 @@ namespace CalamityOverhaul
             //Sandevistan.IsActive = !Sandevistan.IsActive;
             //CyberwareUI.Instance.Toggle();
 
-            //if (player.altFunctionUse == 0) {
-            //    Cyberspace.Activate(player);   // 展开领域
-            //    Cyberspace.SetLayer(3, player);
-            //}
-            //else {
-            //    Cyberspace.Deactivate(); // 收缩关闭
-            //}
+            if (player.altFunctionUse == 0) {
+                Cyberspace.Activate(player);   // 展开领域
+                Cyberspace.SetLayer(3, player);
+            }
+            else {
+                Cyberspace.Deactivate(); // 收缩关闭
+            }
 
             HackTime.InfiniteHack = true;
 
