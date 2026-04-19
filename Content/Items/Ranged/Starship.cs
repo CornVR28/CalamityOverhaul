32d36d5747967d0ee7c33195ca428dcf7c65f5fd
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-04-19 17:14:45 +0800

    Create Starship.cs

---
+++ Content/Items/Ranged/Starship.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.Items.Ranged
+{
+    internal class Starship : ModItem
+    {
+        public override string Texture => CWRConstant.Item_Ranged + "Starship";
+    }
+}
